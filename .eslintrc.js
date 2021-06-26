fe2ef83c35c191f83f35d4c38b9b98b67c5a6593
thinhpq <thinhpq@its-global.vn>
2021-06-27 01:08:37 +0700

    build

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,20 +2,20 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    node: true
+    node: true,
   },
   extends: ["standard"],
   globals: {
     Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
+    SharedArrayBuffer: "readonly",
   },
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: "module"
+    sourceType: "module",
   },
   plugins: ["react"],
-  rules: {}
+  rules: {},
 };
--- src/ckeditor.js
+++ src/ckeditor.js
@@ -234,19 +234,59 @@ BaseEditor.defaultConfig = {
   language: "vi",
   codeBlock: {
     languages: [
-      { language: "plaintext", label: "Plain text" }, // The default language.
-      { language: "c", label: "C" },
-      { language: "cs", label: "C#" },
-      { language: "cpp", label: "C++" },
-      { language: "css", label: "CSS" },
-      { language: "diff", label: "Diff" },
-      { language: "xml", label: "HTML/XML" },
-      { language: "java", label: "Java" },
-      { language: "javascript", label: "JavaScript" },
-      { language: "php", label: "PHP" },
-      { language: "python", label: "Python" },
-      { language: "ruby", label: "Ruby" },
-      { language: "typescript", label: "TypeScript" },
+      {
+        language: "plaintext",
+        label: "Plain text",
+      },
+      {
+        // The default language.
+        language: "c",
+        label: "C",
+      },
+      {
+        language: "cs",
+        label: "C#",
+      },
+      {
+        language: "cpp",
+        label: "C++",
+      },
+      {
+        language: "css",
+        label: "CSS",
+      },
+      {
+        language: "diff",
+        label: "Diff",
+      },
+      {
+        language: "xml",
+        label: "HTML/XML",
+      },
+      {
+        language: "java",
+        label: "Java",
+      },
+      {
+        language: "javascript",
+        label: "JavaScript",
+      },
+      {
+        language: "php",
+        label: "PHP",
+      },
+      {
+        language: "python",
+        label: "Python",
+      },
+      {
+        language: "ruby",
+        label: "Ruby",
+      },
+      {
+        language: "typescript",
+        label: "TypeScript",
+      },
     ],
   },
 };
